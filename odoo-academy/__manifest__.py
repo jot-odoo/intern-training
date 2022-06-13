cb38e8d52bc6bee1457459312a8047f73694aca1
John Truong <trucojo@gmail.com>
2022-06-13 14:12:53 -0400

    [ADD] odoo-academy

---
+++ odoo-academy/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Academy",
+
+    'summary': """""",
+
+    'description': """
+        []
+        """,
+
+    'author': 'Odoo Inc',
+    'website': 'https://www.odoo.com/',
+
+    'category': 'Custom Development',
+    'version': '1.0',
+    'license': 'OPL-1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [],
+    'application': False,
+}
